4f6959d32e4dc49853a0cbb29abe62ee008f0570
7ach7ouch101 <69723977+7ach7ouch101@users.noreply.github.com>
2023-02-12 22:28:15 +0100

    Add files via upload

--- cpp/ex01/PhoneBook.hpp
+++ cpp/ex01/PhoneBook.hpp
@@ -17,19 +17,29 @@ void    PhoneBook::addContact(int i)
 {
     std::string first_name, last_name, nick_name, phone_number, darket_secret;
     std::cout << "Enter your first name please: " << std::endl;
-    std::cin >> first_name;
+    getline(std::cin, first_name);
+    if (std::cin.eof())
+        exit(1);
     contact[i].set_contact_firstname(first_name);
     std::cout << "Enter your last name please: " << std::endl;
-    std::cin >> last_name;
+    getline(std::cin, last_name);
+    if (std::cin.eof())
+        exit(1);
     contact[i].set_contact_lastname(last_name);
     std::cout << "Enter your nickname please: " << std::endl;
-    std::cin >> nick_name;
+    getline(std::cin, nick_name);
+    if (std::cin.eof())
+        exit(1);
     contact[i].set_contact_nick_name(nick_name);
     std::cout << "Enter your phone number please: " << std::endl;
-    std::cin >> phone_number;
+    getline(std::cin, phone_number);
+    if (std::cin.eof())
+        exit(1);
     contact[i].set_contact_phone_number(phone_number);
     std::cout << "Enter your darket secret please: " << std::endl;
-    std::cin >> darket_secret;
+    getline(std::cin, darket_secret);
+    if (std::cin.eof())
+        exit(1);
     contact[i].set_contact_darket_secret(darket_secret);
 }
 
@@ -45,7 +55,7 @@ void    PhoneBook::display(int i)
 
 void    PhoneBook::displayinfo(int num_input, int i)
 {
-    //atoi
+    std::cout << "hazakiiiiiiii." << std::endl;
     if(num_input >= i || num_input < 0)
     {
         std::cout << "Please enter a valid index." << std::endl;
@@ -56,5 +66,6 @@ void    PhoneBook::displayinfo(int num_input, int i)
     std::cout << "Nickname : " << contact[num_input].get_nick_name() << std::endl;
     std::cout << "Phonenumber : " << contact[num_input].get_phone_number() << std::endl;
     std::cout << "Darkest secret : " << contact[num_input].get_darket_secret() << std::endl;
+    return ;
 }
 #endif
--- cpp/ex01/main.cpp
+++ cpp/ex01/main.cpp
@@ -14,31 +14,47 @@ int main(int ac, char **av)
     PhoneBook phone;
     Contact con;
     std::string input;
-    int num_input;
+    std::string num_input;
     int i = 0;
+    int index = 0;
     while(true && ac == 1)
     {
+        // if (std::cin.eof() || std::cin.fail())
+        // {
+        //     std::clearerr(stdin);
+        //     std::cin.clear();
+        //     std::cout << std::endl;
+        // }
         std::cout << "Enter a command please: (ADD/EXIT/SEARCH) : ";
-        std::cin >> input;
-        if((ToUpper(input)) == "ADD")
-        {
+        std::getline(std::cin, input);
+        // if (!input[0])
+        //     continue;
+        if (std::cin.eof())
+                exit(1);
+        if(input == "ADD")
             phone.addContact(i);
-        }
-        else if((ToUpper(input)) == "SEARCH")
+        else if(input == "SEARCH")
         {
             phone.display(i);
             std::cout << "Please enter a valid index:" << std::endl;
-            std::cin >> num_input;
-            phone.displayinfo(num_input, i);
+            std::getline(std::cin, num_input);
+            if (std::cin.eof())
+                exit(1);
+            if (num_input[0] && isdigit(num_input[0]))
+            {
+                index = std::stoi(num_input);
+                std::cout << index << "++++++" << std::endl;
+                phone.displayinfo(index, i);
+            }
         }
-        else if((ToUpper(input)) == "EXIT")
+        else if(input == "EXIT")
         {
             std::cout << "BYEEEE ;)" << std::endl;
             exit(0);
         }
-        else
+        else if(input != "ADD" || input != "EXIT" || input != "SEARCH")
             std::cout << "Enter a valid command please" << std::endl;
-        if((ToUpper(input)) == "ADD")
+        if(input == "ADD")
             i++;
         if(i == 8)
             i = 0;
